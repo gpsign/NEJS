acb45e4959919b1836e971abb93e93588617c2cf
gabriel.signorini <gabriel.signorini@makertec.com.br>
2023-11-28 12:38:15 -0300

    Feat: custom tile render logic

--- src/scripts/classes/Tile.js
+++ src/scripts/classes/Tile.js
@@ -4,15 +4,57 @@ export default class TileClass {
 
 		const aux = this.sprite.data;
 
-		for (let i = 0; i < aux.length; i += 4) {
-			aux[i] = 255;
-            aux[i + 1] = 0;
-            aux[i + 2] = 0;
+		const tp = [
+			[0, 0, 0, 0],
+			[0, 0, 0, 255],
+			[255, 255, 0, 255],
+			[165, 42, 42, 255],
+		];
+
+		const teste = [
+			[2, 2, 2, 2, 2, 2, 2, 2],
+			[3, 3, 3, 3, 3, 3, 3, 3],
+			[3, 3, 1, 3, 3, 1, 3, 3],
+			[3, 3, 1, 3, 3, 1, 3, 3],
+			[3, 1, 3, 3, 3, 3, 1, 3],
+			[3, 3, 1, 3, 3, 1, 3, 3],
+			[3, 3, 3, 1, 1, 3, 3, 3],
+			[3, 3, 3, 3, 3, 3, 3, 3],
+		];
+
+		const ref = [];
+
+		unclamp(teste, ref);
+		console.log(ref);
+
+		let bitCounter = 0;
+
+		for (let i = 0; i < ref.length; i++) {
+			const pData = ref[i];
+			const color = tp[pData]
+
+
+			console.log(color)
+
+			aux[bitCounter] = color[0];
+			aux[bitCounter + 1] = color[1];
+			aux[bitCounter + 2] = color[2];
+			aux[bitCounter + 3] = color[3];
+			bitCounter += 4;
 		}
 
-		console.log(this.sprite);
+		console.log(aux);
+
+		this.sprite.data.set(aux);
 	}
 	render(p) {
-		p.putImageData(this.sprite, 130, 150);
+		p.putImageData(this.sprite, 133, 153);
 	}
+}
+
+function unclamp(arr, ref) {
+	arr.forEach((value) => {
+		if (value.push) unclamp(value, ref);
+		else ref.push(value);
+	});
 }
