385357f8a0cb86d2d137969ba5e077b132fda77d
gabriel.signorini <gabriel.signorini@makertec.com.br>
2023-11-29 08:15:09 -0300

    Fix: color allocation

--- src/scripts/classes/Color.js
+++ src/scripts/classes/Color.js
@@ -1,7 +1,8 @@
 export default class ColorClass {
 	constructor(name, r = 0, g = 0, b = 0, a = 255) {
+		if (a > 0) a = 255;
 		this.r = r;
-		this.g = r;
+		this.g = g;
 		this.b = b;
 		this.a = a;
 		this.name = name;
--- src/scripts/classes/Tile.js
+++ src/scripts/classes/Tile.js
@@ -1,31 +1,53 @@
 import { unfold } from "../utils.js";
 
 export default class TileClass {
-	constructor(p, data, pallete, mirrored = false) {
+	constructor(p, data, pallete) {
 		this.sprite = p.createImageData(8, 8);
+		this.pallete = pallete;
+		this.data = data;
 
 		const aux = this.sprite.data;
 		const unfoldedData = unfold(data);
 
 		let bitCounter = 0;
 
-
 		for (let i = 0; i < unfoldedData.length; i++) {
 			const pData = unfoldedData[i];
 			const color = pallete.data[pData];
 
 			aux[bitCounter] = color.r;
-			aux[bitCounter + 1] = color.b;
-			aux[bitCounter + 2] = color.g;
+			aux[bitCounter + 1] = color.g;
+			aux[bitCounter + 2] = color.b;
 			aux[bitCounter + 3] = color.a;
 			bitCounter += 4;
 		}
 
-		console.log(aux);
-
+		console.log(bitCounter);
 		this.sprite.data.set(aux);
 	}
 	render(p) {
 		p.putImageData(this.sprite, 133, 153);
+	}
+	mirror() {
+		const aux = this.sprite.data;
+		const unfoldedData = unfold(this.data);
+		
+
+		let bitCounter = 256;
+
+		for (let i = unfoldedData.length - 1; i >= 0; i--) {
+			const pData = unfoldedData[i];
+			const color = this.pallete.data[pData];
+
+			console.log(color)
+
+			aux[bitCounter] = color.r;
+			aux[bitCounter + 1] = color.g;
+			aux[bitCounter + 2] = color.b;
+			aux[bitCounter + 3] = color.a;
+			bitCounter -= 4;
+		}
+
+		this.sprite.data.set(aux);
 	}
 }
